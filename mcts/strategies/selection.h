c0724f186dc32b5df042af0869c91e1429699449
072jiajia <072jiajia@gmail.com>
2022-12-25 20:23:59 +0800

    Move strategy patterns of mcts to mcts/strategies/*

---
+++ mcts/strategies/selection.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "../utils.h"
+
+class MCTSNode_;
+class RaveNode;
+class MCTSMutexNode;
+
+class SelectionStrategy
+{
+public:
+    virtual ~SelectionStrategy() = default;
+    virtual int Select(MCTSNode_ *node) const = 0;
+};
+
+class UCBHighest : public SelectionStrategy
+{
+public:
+    UCBHighest(float C = 1.4);
+
+    int Select(MCTSNode_ *node) const;
+
+private:
+    float C_;
+};
+
+class RaveUCBHighest : public SelectionStrategy
+{
+public:
+    RaveUCBHighest(float C = 1.4);
+
+    int Select(MCTSNode_ *node) const;
+
+private:
+    float C_;
+};
+
+class UCBHighestVirtualLoss : public SelectionStrategy
+{
+public:
+    UCBHighestVirtualLoss(float C = 1.4);
+
+    int Select(MCTSNode_ *node) const;
+
+private:
+    float C_;
+};
